dc30a961a4135bab9a71cbb94f1e7ee824508f36
Andries van Renssen <andries.vanrenssen@gellish.net>
2018-10-16 00:20:50 +0200

    Clean up module Mapping_tables_IB

--- Web-Communicator/Mapping_tables_IB.py
+++ Web-Communicator/Mapping_tables_IB.py
@@ -4,26 +4,31 @@
 '''
 
 # Main keys list
-#                   IB               Gellish
-keys_map       = [('articleId'     ,970047, 'identificatie'),\
-                  ('description'   ,5605  , 'synonieme aanduiding'),\
-                  ('properties'    ,493676, 'verzameling uitdrukkingen')]
+#             IB          Gellish
+keys_map = [('articleId', 970047, 'identificatie'),\
+            ('description', 5605, 'synonieme aanduiding'),\
+            ('properties', 493676, 'verzameling uitdrukkingen')]
 
 # Attributes of 'properties'
-#                   IB                Gellish                 Gellish                                             Gellish    IB      IB
-attributes_map = [('ibProductsoort',('identificatie', 5396  , 'is een model'                      , 6066, 730066, 'soort object', '', ''   )),\
-                  ('breedteMm'     ,('identificatie', 5527  , 'heeft per definitie als aspect een', 6066, 550464, 'breedte', 'value','unit')),\
-                  ('hoogteMm'      ,('identificatie', 5527  , 'heeft per definitie als aspect een', 6066, 550126, 'hoogte' , 'value','unit')),\
-                  ('gewichtKg'     ,('identificatie', 5527  , 'heeft per definitie als aspect een', 6066, 550047, 'gewicht', 'value','unit'))]
+#                   IB                Gellish                 Gellish
+attributes_map = [('ibProductsoort', ('identificatie', 5396, 'is een model',
+                                      #Gellish       Gellish        IB  IB
+                                      6066, 730066, 'soort object', '', '')),
+                  ('breedteMm', ('identificatie', 5527, 'heeft per definitie als aspect een',
+                                 6066, 550464, 'breedte', 'value','unit')),
+                  ('hoogteMm', ('identificatie', 5527, 'heeft per definitie als aspect een',
+                                6066, 550126, 'hoogte' , 'value','unit')),
+                  ('gewichtKg', ('identificatie', 5527, 'heeft per definitie als aspect een',
+                                 6066, 550047, 'gewicht', 'value','unit'))]
 # Keys for values
-#                   IB                Gellish
-value_key_map  = [('label'          ,910300, 'label'),\
-                  ('name'           ,730066, 'soort'),\
-                  ('value'          ,910130, 'naam'),\
-                  ('unit'           ,1733  , 'schaal')]
+#                  IB     Gellish
+value_key_map = [('label', 910300, 'label'),
+                 ('name', 730066, 'soort'),
+                 ('value', 910130, 'naam'),
+                 ('unit', 1733, 'schaal')]
 
 # Allowed values and units
-#                  IB                Gellish
-values_map     = {'Dakramen|Ramen' :(43769 , 'dakvenster'),\
-                  'mm'             :(570423, 'mm'),\
-                  'kg'             :(570039, 'kg')}
+#              IB               Gellish
+values_map = {'Dakramen|Ramen':(43769 , 'dakvenster'),
+              'mm':(570423, 'mm'),
+              'kg':(570039, 'kg')}
